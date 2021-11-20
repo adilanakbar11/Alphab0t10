e7564b4d189a9148608ef08d87192eccaf9a6306
adilanakbar11 <91605602+adilanakbar11@users.noreply.github.com>
2021-11-20 13:12:04 +0700

    Update allpayment.js

--- shop/allpayment.js
+++ shop/allpayment.js
@@ -1,21 +1,21 @@
 const allpayment = (prefix) => {
-return `PAYMENT BY ZEEONE OFC
+return `𝗣𝗔𝗬𝗠𝗘𝗡𝗧 𝗕𝗬 𝗟𝗔𝗫𝗬 𝗦𝗧𝗢𝗥𝗘 💸
 
 *さ BANK*
 1. BRI 
-	ㅁ 5074-01-003141-52-8
+	ㅁ TIDAK ADA ❌
 		• R***A*DI
 		
 *さ E-MONEY*
 1. GOPAY
-	ㅁ 0887 4350 47326
-		• ZE**** **C
+	ㅁ 082117489822
+		• AD**** A***R
 2. OVO
-	ㅁ 0887 4350 47326
-		• EL*****TI
+	ㅁ 082117489822
+		• TA*****H
 3. DANA
-	ㅁ 0887 4350 47326
-		• ZE**** **C
+	ㅁ 082117489822
+		• TA*****H
 
 *さ PULSA*
 
